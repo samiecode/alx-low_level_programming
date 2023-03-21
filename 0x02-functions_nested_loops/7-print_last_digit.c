98df8ae29da67fb5c6f09783aeb56035da63fe15
samieteq <codewithsamie@gmail.com>
2023-03-21 12:22:00 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_last_digit - prints the last digit of a number
  *
@@ -9,6 +9,11 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	printf("%d\n", n);
+	last_digit = (n % 10);
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
