54e80b74d1f2ec1ed7813c54c53ed7936ceba683
samieteq <codewithsamie@gmail.com>
2023-03-27 08:30:51 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string -  reverses a string.
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+	for (i = len - 1;  i > -1; i--)
+	{
+/*		_putchar(s[i]);*/
+	}
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
